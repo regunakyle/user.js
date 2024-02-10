b48349b5edf16a4d3ca5a9e0db0a65144397fd6f
Reguna <60984878+regunakyle@users.noreply.github.com>
2024-02-11 00:02:29 +0800

    Set about:blank for new pages

--- user-overrides.js
+++ user-overrides.js
@@ -3,14 +3,6 @@
  * [NOTE] Session Restore is cleared with history (2811), and not used in Private Browsing mode
  * [SETTING] General>Startup>Restore previous session ***/
 user_pref("browser.startup.page", 3);
-/* 0103: set HOME+NEWWINDOW page
- * about:home=Firefox Home (default, see 0105), custom URL, about:blank
- * [SETTING] Home>New Windows and Tabs>Homepage and new windows ***/
-user_pref("browser.startup.homepage", "about:home");
-/* 0104: set NEWTAB page
- * true=Firefox Home (default, see 0105), false=blank page
- * [SETTING] Home>New Windows and Tabs>New tabs ***/
-user_pref("browser.newtabpage.enabled", true);
 
 /** SANITIZE ON SHUTDOWN: IGNORES "ALLOW" SITE EXCEPTIONS ***/
 /* 2811: set/enforce what items to clear on shutdown (if 2810 is true) [SETUP-CHROME]
